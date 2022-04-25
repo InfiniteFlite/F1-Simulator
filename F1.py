40d10404324de884a8a7183e7b794c3b8f165975
InfiniteFlite <infinate1234flight@gmail.com>
2022-04-24 22:34:19 -0700

    boiler plate stuff

--- F1.py
+++ F1.py
@@ -1,1 +1,14 @@
+from flask import Flask, url_for, render_template, request, session, redirect
 
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template("home.html")
+
+@app.route("/simsetup")
+def render_setup():
+    return render_template("setup.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
