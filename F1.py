46222a8eae963d82141d2a7f316efef641ada111
InfiniteFlite <infinate1234flight@gmail.com>
2022-04-24 23:37:25 -0700

    idk I guess I made progress

--- F1.py
+++ F1.py
@@ -1,6 +1,17 @@
 from flask import Flask, url_for, render_template, request, session, redirect
+import math
+import random
 
 app = Flask(__name__)
+
+constructors = ["RedBull", "Ferrari", "Mercedes", "Mclaren", "Haas", "Alpha Tauri", "Alfa Romeo", "Alpine", "Aston Martin", "Williams"]
+
+def randomize_constructors_for_form():
+    random.shuffle(constructors)
+    options = ""
+    for c in constructors:
+        options+= c + ", "
+    return options
 
 @app.route("/")
 def render_main():
@@ -8,7 +19,12 @@ def render_main():
 
 @app.route("/simsetup")
 def render_setup():
-    return render_template("setup.html")
+    c = randomize_constructors_for_form()
+    return render_template("setup.html", constructorListValues = c)
+
+@app.route("/sim")
+def render_sim():
+    return render_template("sim.html")
 
 if __name__=="__main__":
     app.run(debug=True)
